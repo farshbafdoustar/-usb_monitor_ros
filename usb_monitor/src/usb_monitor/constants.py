099646c35cb7de3835ec8b0bcee51887fb125ef6
Behnoosh <behnoosh.sepehrian@gmail.com>
2022-10-12 14:09:18 +0200

    the name policy in the path had conflict with Graph Resource Names in ROS, the usb-monitor-sda1 chaged to usb_monitor/-sda1

--- usb_monitor/src/usb_monitor/constants.py
+++ usb_monitor/src/usb_monitor/constants.py
@@ -7,4 +7,4 @@ USB_FILE_SYSTEM_SUBSCRIBE_SERVICE_NAME = "usb_file_system_subscribe"
 USB_MOUNT_POINT_MANAGER_SERVICE_NAME = "usb_mount_point_manager"
 
 # Base mount point.
-BASE_MOUNT_POINT = os.path.join(os.sep, "mnt", "usb-monitor")
+BASE_MOUNT_POINT = os.path.join(os.sep, "mnt", "usb_monitor")
